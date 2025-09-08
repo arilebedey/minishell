d5f681d427cc54e651962ac7054f1a3f2ce26f18
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-09-08 11:06:21 +0200

    fix: unfork heredocs

--- minishell/exec/include/heredoc.h
+++ minishell/exec/include/heredoc.h
@@ -3,29 +3,18 @@
 
 # include "../../include/command.h"
 
-typedef struct s_heredoc_ctx
-{
-	t_infile			*infile;
-	t_command			*head_cmd;
-	char				*tmpname;
-}						t_heredoc_ctx;
-
-extern t_heredoc_ctx	*g_heredoc_ctx;
-
 // process.c
-int						process_infiles(t_command *head_cmd);
-void					cleanup_infiles(t_command *head_cmd);
+int		process_infiles(t_command *head_cmd);
+void	cleanup_infiles(t_command *head_cmd);
 
 // process_heredocs.c
-void					replace_first_heredoc_with_file(t_command *cmd,
-							char *filename);
-int						open_temp_infile(char **filename, int index);
+void	replace_first_heredoc_with_file(t_command *cmd, char *filename);
+int		open_temp_infile(char **filename, int index);
 
-// write_heredocs.c
-int						write_heredocs_to_file(t_command *cmd, int fd,
-							char *filename);
-int						write_heredoc_to_fd(t_infile *infile, int fd,
-							char *tmpname, t_command *head_cmd);
-char					*generate_heredoc_filename(int index);
+// write_heredoc.c
+int		write_heredocs_to_file(t_command *cmd, int fd, char *filename);
+int		write_heredoc_to_fd(t_infile *infile, int fd, char *tmpname,
+			t_command *head_cmd);
+char	*generate_heredoc_filename(int index);
 
 #endif
--- minishell/exec/src/heredocs/write_heredoc.c
+++ minishell/exec/src/heredocs/write_heredoc.c
@@ -1,5 +1,4 @@
 #include "../../../include/command.h"
-#include "../../../include/env.h"
 #include "../../../include/sig/sig.h"
 #include "../../../libft/libft.h"
 #include "../../include/heredoc.h"
@@ -9,11 +8,15 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
-extern t_env	*g_head_env;
-t_heredoc_ctx	*g_heredoc_ctx = NULL;
+static void	heredoc_sigint(int signum)
+{
+	(void)signum;
+	g_exit_status = 130;
+	close(STDIN_FILENO);
+	write(STDOUT_FILENO, "\n", 1);
+}
 
 char	*generate_heredoc_filename(int index)
 {
@@ -38,29 +41,27 @@ char	*generate_heredoc_filename(int index)
 	return (filename);
 }
 
-static void	heredoc_sigint(int signum)
-{
-	(void)signum;
-	write(STDOUT_FILENO, "\n", 1);
-	rl_replace_line("", 0);
-	free_env_list(g_head_env);
-	if (g_heredoc_ctx && g_heredoc_ctx->tmpname)
-		free(g_heredoc_ctx->tmpname);
-	if (g_heredoc_ctx && g_heredoc_ctx->head_cmd)
-		free_cmd_list(&g_heredoc_ctx->head_cmd);
-	exit(130);
-}
-
-static void	child_write_heredoc(t_infile *infile, int fd, char *tmpname,
+int	write_heredoc_to_fd(t_infile *infile, int fd, char *tmpname,
 		t_command *head_cmd)
 {
-	char	*line;
+	struct sigaction	orig_int;
+	struct sigaction	act_int;
+	int					save_stdin;
+	char				*line;
 
-	g_heredoc_ctx->infile = infile;
-	g_heredoc_ctx->head_cmd = head_cmd;
-	g_heredoc_ctx->tmpname = tmpname;
-	signal(SIGINT, heredoc_sigint);
-	signal(SIGQUIT, SIG_IGN);
+	(void)tmpname;
+	(void)head_cmd;
+	g_exit_status = 0;
+	save_stdin = dup(STDIN_FILENO);
+	if (save_stdin < 0)
+		return (perror("dup stdin"), 0);
+	sigaction(SIGINT, NULL, &orig_int);
+	act_int = orig_int;
+	act_int.sa_handler = heredoc_sigint;
+	act_int.sa_flags |= SA_RESTART;
+	sigemptyset(&act_int.sa_mask);
+	if (sigaction(SIGINT, &act_int, NULL) == -1)
+		return (perror("sigaction heredoc"), close(save_stdin), 0);
 	while (1)
 	{
 		line = readline("heredoc> ");
@@ -75,41 +76,11 @@ static void	child_write_heredoc(t_infile *infile, int fd, char *tmpname,
 		write(fd, "\n", 1);
 		free(line);
 	}
-	close(fd);
-	free(tmpname);
-	free_cmd_list(&head_cmd);
-	free_env_list(g_head_env);
-	exit(0);
-}
-
-int	write_heredoc_to_fd(t_infile *infile, int fd, char *tmpname,
-		t_command *head_cmd)
-{
-	pid_t			pid;
-	int				status;
-	t_heredoc_ctx	ctx;
-
-	ctx.infile = infile;
-	ctx.head_cmd = head_cmd;
-	ctx.tmpname = tmpname;
-	g_heredoc_ctx = &ctx;
-	pid = fork();
-	if (pid < 0)
-		return (perror("fork heredoc"), 0);
-	if (pid == 0)
-		child_write_heredoc(infile, fd, tmpname, head_cmd);
-	if (!init_parent_sigaction())
+	dup2(save_stdin, STDIN_FILENO);
+	close(save_stdin);
+	sigaction(SIGINT, &orig_int, NULL);
+	if (g_exit_status == 130)
 		return (0);
-	if (waitpid(pid, &status, 0) < 0)
-		return (perror("waitpid heredoc"), 0);
-	if (!init_interactive_sigaction())
-		return (0);
-	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGINT)
-	{
-		g_exit_status = 130;
-		write(STDOUT_FILENO, "\n", 1);
-		return (0);
-	}
 	return (1);
 }
 
@@ -118,6 +89,7 @@ int	write_heredocs_to_file(t_command *cmd, int fd, char *filename)
 	t_infile	*in;
 	int			has_heredoc;
 
+	(void)filename;
 	in = cmd->head_infile;
 	has_heredoc = 0;
 	while (in)
