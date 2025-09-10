e8c1753a2dfd03e6e7e5f63f0b9000d04a2cb0b3
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-09-10 11:23:27 +0200

    chore: split main for norm + `make` now detects .h changes

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -10,13 +10,32 @@
 
 #define PROMPT "\001\033[1;31m\002p\001\033[1;33m\002e\001\033[1;33m\002t\001\033[1;32m\002i\001\033[1;34m\002t\001\033[1;35m\002s\001\033[1;31m\002h\001\033[1;33m\002e\001\033[1;32m\002l\001\033[1;34m\002l\001\033[0m\002> "
 
+static int	run_shell_loop(t_env **head_env)
+{
+	char		*line;
+	t_command	*head_cmd;
+	int			exec_result;
+	int			sig_result;
+
+	line = readline(PROMPT);
+	while (line)
+	{
+		add_history(line);
+		head_cmd = parse(line, *head_env);
+		free(line);
+		exec_result = exec(head_cmd, *head_env);
+		sig_result = init_interactive_sigaction();
+		free_cmd_list(&head_cmd);
+		if (!exec_result || !sig_result)
+			return (0);
+		line = readline(PROMPT);
+	}
+	return (1);
+}
+
 int	main(int ac, char **av, char **envp)
 {
-	char				*line;
-	static t_env		*head_env = NULL;
-	static t_command	*head_cmd = NULL;
-	int					exec_result;
-	int					sig_result;
+	static t_env	*head_env = NULL;
 
 	(void)ac;
 	(void)av;
@@ -25,19 +44,7 @@ int	main(int ac, char **av, char **envp)
 	head_env = init_env_list(envp);
 	if (!head_env)
 		return (1);
-	line = readline(PROMPT);
-	while (line)
-	{
-		add_history(line);
-		head_cmd = parse(line, head_env);
-		free(line);
-		exec_result = exec(head_cmd, head_env);
-		sig_result = init_interactive_sigaction();
-		free_cmd_list(&head_cmd);
-		if (!exec_result || !sig_result)
-			break ;
-		line = readline(PROMPT);
-	}
-	ft_printf("exit\n");
-	return (free_cmd_list(&head_cmd), free_env_list(head_env), 0);
+	if (!run_shell_loop(&head_env))
+		ft_printf("exit\n");
+	return (free_env_list(head_env), 0);
 }
