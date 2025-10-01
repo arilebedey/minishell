7303bb58b1a28545a006ad1fc6d4f36d0ab8a69e
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-10-01 09:03:05 +0200

    feat: echo -nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

--- exec/src/builtins/b_echo.c
+++ exec/src/builtins/b_echo.c
@@ -6,7 +6,7 @@
 /*   By: alebedev <alebedev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 13:03:42 by alebedev          #+#    #+#             */
-/*   Updated: 2025/09/11 13:03:47 by alebedev         ###   ########.fr       */
+/*   Updated: 2025/10/01 08:51:50 by alebedev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 #include "../../../include/env.h"
 #include "../../../include/sig/sig.h"
 #include "../../../libft/libft.h"
+
+static int	is_n_flag(const char *s)
+{
+	int	i;
+
+	if (!s || s[0] != '-' || s[1] != 'n')
+		return (0);
+	i = 1;
+	while (s[++i])
+	{
+		if (s[i] != 'n')
+			return (0);
+	}
+	return (1);
+}
 
 int	builtin_echo(t_command *cmd, t_env *head_env)
 {
@@ -25,7 +40,7 @@ int	builtin_echo(t_command *cmd, t_env *head_env)
 	if (arg)
 		arg = arg->next;
 	print_newline = 1;
-	if (arg && !ft_strncmp(arg->value, "-n", 3))
+	while (arg && is_n_flag(arg->value))
 	{
 		print_newline = 0;
 		arg = arg->next;
